f1f5addbebb747a78834c45ab1bd93493e4e1a85
LeadDev <leaddev@anon.farm>
2021-10-23 21:07:56 +0530

    test: add position creation checks

--- test/LiquidityPosition.spec.ts
+++ test/LiquidityPosition.spec.ts
@@ -3,7 +3,7 @@ import hre from 'hardhat';
 
 import { LiquidityPositionTest } from '../typechain';
 
-describe('Liquidity Position', () => {
+describe('Liquidity Position Library', () => {
   let test: LiquidityPositionTest;
 
   beforeEach(async () => {
@@ -50,6 +50,24 @@ describe('Liquidity Position', () => {
     it('negative.negative#2', async () => {
       const concatenated = await test.assertConcat(-1 * 2 ** 23, -1 * 2 ** 23);
       expect(concatenated).to.eq(0x800000_800000);
+    });
+  });
+
+  describe('#create', () => {
+    it('empty', async () => {
+      expect(await test.isPositionActive(-1, 1)).to.be.false;
+
+      await test.createEmptyPosition(-1, 1);
+      const position = await test.callStatic.createEmptyPosition(-1, 1);
+
+      expect(await test.isPositionActive(-1, 1)).to.be.true;
+      expect(position.tickLower).to.eq(-1);
+      expect(position.tickUpper).to.eq(1);
+      expect(position.liquidity).to.eq(0);
+    });
+
+    it('invalid', async () => {
+      expect(test.createEmptyPosition(1, -1)).to.be.revertedWith('InvalidTicks(1, -1)');
     });
   });
 });
