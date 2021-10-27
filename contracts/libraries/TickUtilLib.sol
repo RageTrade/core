c8988c2596cea4243a21913e374b9ef766d08efc
FinDev <findev@anon.farm>
2021-10-27 16:19:18 +0530

    Fix: Fixed issues with GlobalUtilLib (Stack Too Deep) and TickUtilLib (updating sumA before sumFP)

--- contracts/libraries/TickUtilLib.sol
+++ contracts/libraries/TickUtilLib.sol
@@ -14,13 +14,11 @@ library TickUtilLib {
         uint256 feeGrowthOutsideShortsX128; // see if binary fixed point is needed
     }
 
-    function cross(TickState storage tick, GlobalUtilLib.GlobalState storage global) external{
-    //sumFP should be updated before updating sumB and lastTradeTS  
-
-    tick.sumA = global.sumA; //Need not extrapolate because sumA would be updated just before tick cross based on remaining amount
-    tick.sumBOutside = global.sumB - tick.sumBOutside;
-    tick.sumFPOutside = global.sumFP - global.getExtrapolatedSumFP(tick.sumA, tick.sumBOutside, tick.sumFPOutside, block.timestamp);
-
-    tick.feeGrowthOutsideShortsX128 = global.feeGrowthGlobalShortsX128 - tick.feeGrowthOutsideShortsX128;
+    function cross(TickState storage tick, GlobalUtilLib.GlobalState storage global) internal{
+        //sumFP should be updated before updating sumB and lastTradeTS  
+        tick.sumFPOutside = global.sumFP - global.getExtrapolatedSumFP(tick.sumA, tick.sumBOutside, tick.sumFPOutside);
+        tick.sumA = global.sumA; //Need not extrapolate because sumA would be updated just before tick cross based on remaining amount
+        tick.sumBOutside = global.sumB - tick.sumBOutside;
+        tick.feeGrowthOutsideShortsX128 = global.feeGrowthGlobalShortsX128 - tick.feeGrowthOutsideShortsX128;
     }
 }
