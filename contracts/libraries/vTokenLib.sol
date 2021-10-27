f152aa0214a4aca87157dbf0ace2396f9278b41e
FinDev <findev@anon.farm>
2021-10-28 03:02:05 +0530

    Mock: Added mock functions to vTokenLib

--- contracts/libraries/vTokenLib.sol
+++ contracts/libraries/vTokenLib.sol
@@ -16,4 +16,18 @@ library VTokenLib {
     function realToken(VToken vToken) internal pure returns (address) {
         return address(0); // TODO implement
     }
+
+    function getVirtualTwapPrice(VToken vToken) internal pure returns(uint160) {
+        return 4000000000000000000000;
+    }
+
+    function getRealTwapPrice(VToken vToken) internal pure returns(uint160) {
+        return 4100000000000000000000;
+    }
+
+    function getVirtualTwapTickIndex(VToken vToken) internal pure returns(int24) {
+        return 1000;
+    }
+
+
 }
