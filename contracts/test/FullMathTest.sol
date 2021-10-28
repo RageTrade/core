aacbe3840579358242ab3a23916956c7ac781d94
LeadDev <leaddev@anon.farm>
2021-10-29 01:54:27 +0530

    test: add checks for FullMath

---
+++ contracts/test/FullMathTest.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import { FullMath } from '../libraries/FullMath.sol';
+
+import { console } from 'hardhat/console.sol';
+
+contract FullMathTest {
+    function mulDiv(
+        uint256 a,
+        uint256 b,
+        uint256 denominator
+    ) public pure returns (uint256 result) {
+        return FullMath.mulDiv(a, b, denominator);
+    }
+
+    function mulDiv(
+        int256 a,
+        int256 b,
+        int256 denominator
+    ) public pure returns (int256 result) {
+        return FullMath.mulDiv(a, b, denominator);
+    }
+
+    function mulDivRoundingUp(
+        uint256 a,
+        uint256 b,
+        uint256 denominator
+    ) public pure returns (uint256 result) {
+        return FullMath.mulDivRoundingUp(a, b, denominator);
+    }
+}
---
+++ test/FullMath.spec.ts
@@ -0,0 +1,115 @@
+import { BigNumber, BigNumberish } from '@ethersproject/bignumber';
+import { expect } from 'chai';
+import { randomBytes } from 'crypto';
+import hre from 'hardhat';
+
+import { FullMathTest } from '../typechain';
+
+interface TestCase {
+  a: BigNumberish;
+  b: BigNumberish;
+  denominator: BigNumberish;
+  expectedResult?: BigNumberish;
+}
+
+describe('FullMath', () => {
+  let test: FullMathTest;
+
+  before(async () => {
+    test = await (await hre.ethers.getContractFactory('FullMathTest')).deploy();
+  });
+
+  describe('#mulDiv(uint256,uint256,uint256)', () => {
+    const cases: Array<TestCase> = [
+      {
+        a: 1,
+        b: 2,
+        denominator: 2,
+        expectedResult: 1,
+      },
+      {
+        a: 1n << 200n,
+        b: 1n << 200n,
+        denominator: 1n << 250n,
+        expectedResult: 1n << 150n,
+      },
+      {
+        a: BigNumber.from(randomBytes(31)),
+        b: BigNumber.from(randomBytes(31)),
+        denominator: BigNumber.from(randomBytes(32)),
+      },
+    ];
+
+    cases.forEach(({ a, b, denominator, expectedResult }) =>
+      it(`${a.toString()} * ${b.toString()} / ${denominator.toString()}`, async () => {
+        const result = await test['mulDiv(uint256,uint256,uint256)'](a, b, denominator);
+        expect(result).to.eq(expectedResult ?? BigNumber.from(a).mul(b).div(denominator));
+      }),
+    );
+  });
+
+  describe('#mulDiv(int256,int256,int256)', () => {
+    const cases: Array<TestCase> = [
+      {
+        a: 1,
+        b: 2,
+        denominator: 2,
+        expectedResult: 1,
+      },
+      {
+        a: -1n << 200n,
+        b: 1n << 200n,
+        denominator: 1n << 250n,
+        expectedResult: -1n << 150n,
+      },
+      {
+        a: 1n << 200n,
+        b: -1n << 200n,
+        denominator: 1n << 250n,
+        expectedResult: -1n << 150n,
+      },
+      {
+        a: 1n << 200n,
+        b: 1n << 200n,
+        denominator: -1n << 250n,
+        expectedResult: -1n << 150n,
+      },
+      {
+        a: -1n << 200n,
+        b: -1n << 200n,
+        denominator: 1n << 250n,
+        expectedResult: 1n << 150n,
+      },
+      {
+        a: -1n << 200n,
+        b: 1n << 200n,
+        denominator: -1n << 250n,
+        expectedResult: 1n << 150n,
+      },
+      {
+        a: 1n << 200n,
+        b: -1n << 200n,
+        denominator: -1n << 250n,
+        expectedResult: 1n << 150n,
+      },
+      {
+        a: -1n << 200n,
+        b: -1n << 200n,
+        denominator: -1n << 250n,
+        expectedResult: -1n << 150n,
+      },
+      {
+        a: BigNumber.from(randomBytes(30)).mul(-1),
+        b: BigNumber.from(randomBytes(30)),
+        denominator: BigNumber.from(randomBytes(31)),
+      },
+    ];
+
+    cases.forEach(({ a, b, denominator, expectedResult }) =>
+      it(`${a.toString()} * ${b.toString()} / ${denominator.toString()}`, async () => {
+        const result = await test['mulDiv(int256,int256,int256)'](a, b, denominator);
+        expect(result).to.eq(expectedResult ?? BigNumber.from(a).mul(b).div(denominator));
+      }),
+    );
+  });
+});
