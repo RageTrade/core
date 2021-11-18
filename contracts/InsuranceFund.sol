030a74e808d7883c02cf5dd79aa061960f4f13e3
scdev157 <scdev@anon.farm>
2021-11-18 11:00:41 +0530

    fix: removed console stmts

--- contracts/InsuranceFund.sol
+++ contracts/InsuranceFund.sol
@@ -5,9 +5,7 @@ pragma solidity ^0.8.9;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-
 import './interfaces/IInsuranceFund.sol';
-import 'hardhat/console.sol';
 
 contract InsuranceFund is IInsuranceFund, ERC20 {
     using SafeERC20 for IERC20;
@@ -23,7 +21,6 @@ contract InsuranceFund is IInsuranceFund, ERC20 {
         uint256 totalBase = base.balanceOf(address(this));
         uint256 totalShares = totalSupply();
         uint256 toMint;
-        console.log('Deposit', totalBase, totalShares);
         if (totalShares == 0 || totalBase == 0) {
             toMint = amount;
         } else {
@@ -36,7 +33,6 @@ contract InsuranceFund is IInsuranceFund, ERC20 {
     function withdraw(uint256 shares) external {
         uint256 totalBase = base.balanceOf(address(this));
         uint256 totalShares = totalSupply();
-        console.log('Withdraw', totalBase, totalShares);
         uint256 toWithdraw = (shares * totalBase) / totalShares;
         _burn(msg.sender, shares);
         base.safeTransfer(msg.sender, toWithdraw);
