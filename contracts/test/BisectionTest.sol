8c931cc6f0872c8281c2b87f294a9a706fc3f6e7
LeadDev <leaddev@anon.farm>
2022-02-05 22:00:06 +0530

    feat: add bisection impl

---
+++ contracts/test/BisectionTest.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import { Bisection } from '../libraries/Bisection.sol';
+
+contract BisectionTest {
+    function increasingFunction(uint160 val) public pure returns (uint256) {
+        return (val * 3);
+    }
+
+    function findSolution(
+        uint256 y_target,
+        uint160 x_lower,
+        uint160 x_upper
+    ) external pure returns (uint160) {
+        return Bisection.findSolution(increasingFunction, y_target, x_lower, x_upper);
+    }
+}
