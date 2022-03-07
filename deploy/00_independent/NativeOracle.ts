5b8894d227fdf563b5c1eff7cd7d3b3234de9428
protodev-rage <protodev@rage.trade>
2022-03-07 10:36:23 +0530

    fix: deploy scripts

--- deploy/00_independent/NativeOracle.ts
+++ deploy/00_independent/NativeOracle.ts
@@ -4,21 +4,18 @@ import { getNetworkInfo } from '../network-info'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {
-    deployments: { deploy, save },
+    deployments: { deploy },
     getNamedAccounts,
   } = hre;
 
   const { deployer } = await getNamedAccounts();
 
-  console.log('oracle', getNetworkInfo().ETH_USD_ORACLE)
-
   const deployment = await deploy('NativeOracle', {
     contract: 'ChainlinkOracle',
-    // for rinkeby: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
     args: [
-      '0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8',
-      '18',
-      '6'
+      getNetworkInfo(hre.network.config.chainId).ETH_USD_ORACLE,
+      '18', // native currency decimals
+      '6' // base decimals
     ],
     from: deployer,
     log: true,
