0128d57a9c00b76831bc6b2e0a1a41e663e5f75f
protodev-rage <protodev@rage.trade>
2022-03-09 07:01:45 +0530

    chore: add custom errors instead of require

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -3,5 +3,8 @@
 pragma solidity ^0.8.9;
 
 interface IOracle {
+    error NotEnoughHistory();
+    error ZeroAddress();
+
     function getTwapPriceX128(uint32 twapDuration) external view returns (uint256 priceX128);
 }
