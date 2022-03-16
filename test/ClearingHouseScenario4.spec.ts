0fb40132dbdeae87a1e428d491d990c9946a53f5
FinDev <findev@rage.trade>
2022-03-16 20:51:56 +0530

    test: fixed issues in previous test cases

--- test/ClearingHouseScenario4.spec.ts
+++ test/ClearingHouseScenario4.spec.ts
@@ -815,7 +815,8 @@ describe('Clearing House Scenario 4 (Partial Swaps & Notional Swaps)', () => {
   describe('#Init Params', () => {
     it('Set Params', async () => {
       const liquidationParams = {
-        liquidationFeeFraction: 1500,
+        rangeLiquidationFeeFraction: 1500,
+        tokenLiquidationFeeFraction: 3000,
         insuranceFundFeeShareBps: 5000,
         maxRangeLiquidationFees: 100000000,
         closeFactorMMThresholdBps: 7500,
@@ -842,8 +843,8 @@ describe('Clearing House Scenario 4 (Partial Swaps & Notional Swaps)', () => {
 
       expect(await clearingHouseTest.fixFee()).eq(fixFee);
       expect(protocol.minRequiredMargin).eq(minRequiredMargin);
-      expect(protocol.liquidationParams.liquidationFeeFraction).eq(liquidationParams.liquidationFeeFraction);
-
+      expect(protocol.liquidationParams.rangeLiquidationFeeFraction).eq(liquidationParams.rangeLiquidationFeeFraction);
+      expect(protocol.liquidationParams.tokenLiquidationFeeFraction).eq(liquidationParams.tokenLiquidationFeeFraction);
       expect(protocol.liquidationParams.insuranceFundFeeShareBps).eq(liquidationParams.insuranceFundFeeShareBps);
 
       expect(protocol.removeLimitOrderFee).eq(removeLimitOrderFee);
