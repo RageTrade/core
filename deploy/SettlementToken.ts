d3df7d2aef9c5e0d60c3bbaff1baf73617d951b5
LeadDev <leaddev@anon.farm>
2022-03-25 04:04:15 +0530

    chore: refactor deploy scripts

--- deploy/SettlementToken.ts
+++ deploy/SettlementToken.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { getNetworkInfo } from '../network-info';
-import { IERC20Metadata__factory } from '../../typechain-types';
+import { getNetworkInfo } from './network-info';
+import { IERC20Metadata__factory } from '../typechain-types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {
