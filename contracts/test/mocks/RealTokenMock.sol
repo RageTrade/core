5d7b18fe74340b075762ec3604b491e6d706973f
LeadDev <leaddev@anon.farm>
2022-03-25 04:01:52 +0530

    refactor: remove comment

--- contracts/test/mocks/RealTokenMock.sol
+++ contracts/test/mocks/RealTokenMock.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 
-// pragma solidity ^0.7.6;
-
 // if importing uniswap v3 libraries this might not work
 pragma solidity ^0.8.9;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
