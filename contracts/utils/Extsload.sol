944f5c53087e1c0b530a295f2c21643883526804
LeadDev <leaddev@anon.farm>
2022-03-30 03:44:59 +0530

    feat: add extsload interface

--- contracts/utils/Extsload.sol
+++ contracts/utils/Extsload.sol
@@ -2,8 +2,10 @@
 
 pragma solidity ^0.8.9;
 
+import { IExtsload } from '../interfaces/IExtsload.sol';
+
 /// @notice Allows the contract to make it's state public
-abstract contract Extsload {
+abstract contract Extsload is IExtsload {
     function extsload(bytes32 slot) external view returns (bytes32 val) {
         assembly {
             val := sload(slot)
