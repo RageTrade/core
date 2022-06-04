4c62bfc73523ff7b744c0e6f930a30ac60b11092
0xDosa <findev@rage.trade>
2022-06-04 13:01:39 +0530

    fix: added break while iterating on liquidityPositions (QSP-18, #135)

--- contracts/libraries/LiquidityPositionSet.sol
+++ contracts/libraries/LiquidityPositionSet.sol
@@ -255,6 +255,7 @@ library LiquidityPositionSet {
     function maxNetPosition(LiquidityPosition.Set storage set) internal view returns (uint256 risk) {
         for (uint256 i = 0; i < set.active.length; i++) {
             uint48 id = set.active[i];
+            if (id == 0) break;
             risk += set.positions[id].maxNetPosition();
         }
     }
