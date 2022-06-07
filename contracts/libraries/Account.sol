f2781bff7e638a7ca726d5dacd709bbaf2643ff3
0xDosa <findev@rage.trade>
2022-06-07 19:42:52 +0530

    feat: Event Updates (RT-5, #144)
    Added isSettleProfit to MarginUpdated event
    Removed liquidatorAccountId from TokenPositionLiquidated event

--- contracts/libraries/Account.sol
+++ contracts/libraries/Account.sol
@@ -85,7 +85,7 @@ library Account {
     /// @param accountId serial number of the account
     /// @param collateralId token in which margin is deposited
     /// @param amount amount of tokens deposited
-    event MarginUpdated(uint256 indexed accountId, uint32 indexed collateralId, int256 amount);
+    event MarginUpdated(uint256 indexed accountId, uint32 indexed collateralId, int256 amount, bool isSettleProfit);
 
     /// @notice denotes token position change
     /// @param accountId serial number of the account
@@ -205,13 +205,11 @@ library Account {
     /// @notice denotes token position liquidation event
     /// @dev the selected token position is take from the current account and moved to liquidatorAccount at a discounted prive to current pool price
     /// @param accountId serial number of the account
-    /// @param liquidatorAccountId  account which performed the liquidation
     /// @param poolId id of the rage trade pool for whose position was liquidated
     /// @param keeperFee total liquidaiton fee paid to keeper
     /// @param insuranceFundFee total liquidaiton fee paid to the insurance fund (can be negative in case the account is not enough to cover the fee)
     event TokenPositionLiquidated(
         uint256 indexed accountId,
-        uint256 indexed liquidatorAccountId,
         uint32 indexed poolId,
         int256 keeperFee,
         int256 insuranceFundFee,
@@ -235,7 +233,7 @@ library Account {
         Protocol.Info storage protocol,
         bool checkMargin
     ) external {
-        _updateMargin(account, collateralId, amount, protocol, checkMargin);
+        _updateMargin(account, collateralId, amount, protocol, checkMargin, false);
     }
 
     /// @notice updates 'amount' of profit generated in settlement token
@@ -465,7 +463,7 @@ library Account {
         // deduct liquidation fees from account
         account._updateVQuoteBalance(-(keeperFee + insuranceFundFee));
 
-        emit TokenPositionLiquidated(account.id, 0, poolId, keeperFee, insuranceFundFee, accountMarketValueFinal);
+        emit TokenPositionLiquidated(account.id, poolId, keeperFee, insuranceFundFee, accountMarketValueFinal);
     }
 
     /// @notice removes limit order based on the current price position (keeper call)
@@ -574,13 +572,25 @@ library Account {
         uint32 settlementCollateralId = AddressHelper.truncate(protocol.settlementToken);
         if (profits > 0) {
             account._updateProfit(-profits, protocol, false);
-            account._updateMargin(settlementCollateralId, profits, protocol, false);
+            account._updateMargin({
+                collateralId: settlementCollateralId,
+                amount: profits,
+                protocol: protocol,
+                checkMargin: false,
+                isSettleProfit: true
+            });
         } else if (profits < 0) {
             uint256 balance = account.collateralDeposits.getBalance(settlementCollateralId);
             uint256 profitAbsUint = uint256(-profits);
             uint256 balanceToUpdate = balance > profitAbsUint ? profitAbsUint : balance;
             if (balanceToUpdate > 0) {
-                account._updateMargin(settlementCollateralId, -balanceToUpdate.toInt256(), protocol, false);
+                account._updateMargin({
+                    collateralId: settlementCollateralId,
+                    amount: -balanceToUpdate.toInt256(),
+                    protocol: protocol,
+                    checkMargin: false,
+                    isSettleProfit: true
+                });
                 account._updateProfit(balanceToUpdate.toInt256(), protocol, false);
             }
         }
@@ -618,7 +628,8 @@ library Account {
         uint32 collateralId,
         int256 amount,
         Protocol.Info storage protocol,
-        bool checkMargin
+        bool checkMargin,
+        bool isSettleProfit
     ) internal {
         if (amount > 0) {
             account.collateralDeposits.increaseBalance(collateralId, uint256(amount));
@@ -627,7 +638,7 @@ library Account {
             if (checkMargin) account._checkIfMarginAvailable(true, protocol);
         }
 
-        emit MarginUpdated(account.id, collateralId, amount);
+        emit MarginUpdated(account.id, collateralId, amount, isSettleProfit);
     }
 
     /// @notice updates the vQuote balance for 'account' by 'amount'
