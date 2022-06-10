6dded6d0e6658694c2d317fa9c78a32f0e0ea95a
LeadDev <leaddev@anon.farm>
2022-06-10 10:51:29 +0530

    chore: refactor etherscan-verify script

--- scripts/etherscan-verify.ts
+++ scripts/etherscan-verify.ts
@@ -1,94 +1,88 @@
-import hre, { deployments } from 'hardhat';
+import hre, { deployments, getNamedAccounts } from 'hardhat';
+import { Deployment } from 'hardhat-deploy/types';
 
 import { ClearingHouse__factory, VPoolWrapper__factory } from '../typechain-types';
 
 async function main() {
   const { get } = deployments;
-  const accountLibrary = await get('AccountLibrary');
-  const clearingHouseLogic = await get('ClearingHouseLogic');
-  const vPoolWrapperLogic = await get('VPoolWrapperLogic');
-  const insuranceFundLogic = await get('InsuranceFundLogic');
-  const settlementToken = await get('SettlementToken');
-  const rageTradeFactory = await get('RageTradeFactory');
-  await hreVerify('AccountLibrary', { address: accountLibrary.address });
-  await hreVerify('ClearingHouseLogic', {
-    address: clearingHouseLogic.address,
+
+  const accountLibrary = await hreVerify('AccountLibrary');
+  const clearingHouseLogic = await hreVerify('ClearingHouseLogic', {
     libraries: {
       Account: accountLibrary.address,
     },
   });
-  await hreVerify('VPoolWrapperLogic', { address: vPoolWrapperLogic.address });
-  await hreVerify('InsuranceFundLogic', { address: insuranceFundLogic.address });
-  await hreVerify('SettlementToken', { address: settlementToken.address });
-  await hreVerify('RageTradeFactory', {
-    address: rageTradeFactory.address,
+  const vPoolWrapperLogic = await hreVerify('VPoolWrapperLogic');
+  const insuranceFundLogic = await hreVerify('InsuranceFundLogic');
+  const settlementToken = await hreVerify('SettlementToken');
+  const settlementTokenOracle = await hreVerify('SettlementTokenOracle');
+  const rageTradeFactory = await hreVerify('RageTradeFactory', {
     constructorArguments: [
       clearingHouseLogic.address,
       vPoolWrapperLogic.address,
       insuranceFundLogic.address,
       settlementToken.address,
+      settlementTokenOracle.address,
     ],
   });
 
-  const vQuote = await get('VQuote');
-  const clearingHouse = await get('ClearingHouse');
-  const insuranceFund = await get('InsuranceFund');
-  const proxyAdmin = await get('ProxyAdmin');
-  const settlementTokenOracle = await get('SettlementTokenOracle');
-  await hreVerify('VQuote', { address: vQuote.address, constructorArguments: [6] });
-  const chInitializeData = ClearingHouse__factory.createInterface().encodeFunctionData('__initialize_ClearingHouse', [
+  const proxyAdmin = await hreVerify('ProxyAdmin');
+  const vQuote = await hreVerify('VQuote', { constructorArguments: [6] });
+  const insuranceFund = await hreVerify('InsuranceFund', {
+    constructorArguments: [insuranceFundLogic.address, proxyAdmin.address, '0x'],
+  });
+
+  const { deployer } = await getNamedAccounts();
+  const chInitializeData = ClearingHouse__factory.createInterface().encodeFunctionData('initialize', [
     rageTradeFactory.address,
+    deployer,
+    deployer,
     settlementToken.address,
     settlementTokenOracle.address,
     insuranceFund.address,
     vQuote.address,
   ]);
-  await hreVerify('ClearingHouse', {
-    address: clearingHouse.address,
+  const clearingHouse = await hreVerify('ClearingHouse', {
     constructorArguments: [clearingHouseLogic.address, proxyAdmin.address, chInitializeData],
   });
-  await hreVerify('InsuranceFund', {
-    address: insuranceFund.address,
-    constructorArguments: [insuranceFundLogic.address, proxyAdmin.address, '0x'],
-  });
-  await hreVerify('ProxyAdmin', { address: proxyAdmin.address });
+  await hreVerify('ClearingHouseLens', { constructorArguments: [clearingHouse.address] });
 
-  const swapSimulator = await get('SwapSimulator');
-  await hreVerify('SwapSimulator', { address: swapSimulator.address });
-
-  const ethOracle = await get('ETH-IndexOracle');
-  const ethVPool = await get('ETH-vPool');
-  const ethVPoolWrapper = await get('ETH-vPoolWrapper');
-  const ethVToken = await get('ETH-vToken');
-  await hreVerify('ETH-IndexOracle', { address: ethOracle.address });
-  await hreVerify('ETH-vPool', { address: ethVPool.address });
-  const vpwData = VPoolWrapper__factory.createInterface().encodeFunctionData('__initialize_VPoolWrapper', [
-    {
-      clearingHouse: clearingHouse.address,
-      vToken: ethVToken.address,
-      vQuote: vQuote.address,
-      vPool: ethVPool.address,
-      liquidityFeePips: 1000,
-      protocolFeePips: 500,
-    },
-  ]);
+  await hreVerify('ETH-IndexOracle');
+  const ethVPool = await hreVerify('ETH-vPool');
+  const ethVToken = await hreVerify('ETH-vToken', { constructorArguments: ['Virtual Ether (Rage Trade)', 'vETH', 18] });
   await hreVerify('ETH-vPoolWrapper', {
-    address: ethVPoolWrapper.address,
-    constructorArguments: [vPoolWrapperLogic.address, proxyAdmin.address, vpwData],
-  });
-  await hreVerify('ETH-vToken', {
-    address: ethVToken.address,
-    constructorArguments: ['Virtual Ether (Rage Trade)', 'vETH', 18],
+    constructorArguments: [
+      vPoolWrapperLogic.address,
+      proxyAdmin.address,
+      VPoolWrapper__factory.createInterface().encodeFunctionData('initialize', [
+        {
+          clearingHouse: clearingHouse.address,
+          vToken: ethVToken.address,
+          vQuote: vQuote.address,
+          vPool: ethVPool.address,
+          liquidityFeePips: 1000,
+          protocolFeePips: 500,
+        },
+      ]),
+    ],
   });
 
-  async function hreVerify(label: string, taskArguments: any) {
+  await hreVerify('SwapSimulator');
+
+  // helper method that verify a contract and returns the deployment
+  async function hreVerify(label: string, taskArguments: any = {}): Promise<Deployment> {
     console.log('verifying:', label);
 
+    const deployment = await get(label);
+    taskArguments = { address: deployment.address, ...taskArguments };
+
+    // try to verify on etherscan
     try {
       await hre.run('verify:verify', taskArguments);
     } catch (err: any) {
       console.log(err);
     }
+    return deployment;
   }
 }
 
